6b93a5b63d5696ef3f9b4512b19e4a6d2179e684
COSMIC <93328579+rayavanindra@users.noreply.github.com>
2022-12-17 19:45:22 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/rayavanindra/android_device_xiaomi_sm6250-common -b cherish device/xiaomi/sm6250-common
+git clone https://github.com/rayavanindra/android_device_xiaomi_sm6250-common -b 13 device/xiaomi/sm6250-common
 git clone https://github.com/rayavanindra/android_vendor_xiaomi_sm6250-common -b 13 vendor/xiaomi/sm6250-common 
 git clone https://github.com/rayavanindra/android_vendor_xiaomi_miatoll -b 13 vendor/xiaomi/miatoll 
 #git clone --depth=1 https://github.com/rayavanindra/android_kernel_xiaomi_sm6250 -b main kernel/xiaomi/sm6250
